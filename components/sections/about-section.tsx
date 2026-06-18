1d48382763d75df500e3a186e7c53b3c1881a4ec
beardlaws <officialbeardlaws@gmail.com>
2026-06-18 10:34:22 -0400

    Update about-section.tsx

--- components/sections/about-section.tsx
+++ components/sections/about-section.tsx
@@ -10,7 +10,7 @@ interface AboutSectionProps {
 }
 
 export default function AboutSection({ stats }: AboutSectionProps) {
-  const stopCount = stats?.total ?? 27;
+  const stopCount = stats?.totalStops ?? 0;
 
   return (
     <section id="about" className="py-16 lg:py-24 bg-dark-wood border-t border-border">
